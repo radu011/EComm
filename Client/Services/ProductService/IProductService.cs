c4ef6591e2df538115e64506b83331c482c537f7
radu011 <toaderradu21@gmail.com>
2023-04-07 13:25:19 +0300

    Search Products

--- Client/Services/ProductService/IProductService.cs
+++ Client/Services/ProductService/IProductService.cs
@@ -6,7 +6,10 @@ namespace EComm.Client.Services.ProductService
 	{
 		event Action ProductsChanged;
 		List<Product> Products { get; set; }
+		string Message { get; set; }
 		Task GetProducts(string? categoryUrl = null);
 		Task<ServiceResponse<Product>> GetProduct(int productId);
+		Task SearchProducts(string searchText);
+		Task<List<string>> GetProductSearchSuggestions(string searchText);
 	}
 }
